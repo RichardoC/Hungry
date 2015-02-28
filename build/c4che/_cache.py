640e23c4f26f6e62be9dc0ca8fbf29d0a71209e1
bm026 <bt12753@my.bristol.ac.uk>
2015-02-28 18:51:09 +0000

    wscript file

---
+++ build/c4che/_cache.py
@@ -0,0 +1,6 @@
+BINDIR = '/usr/local/bin'
+DEFINES = ['RELEASE']
+LIBDIR = '/usr/local/lib'
+PEBBLE_SDK_COMMON = '/home/bm/Documents/pebble-dev/PebbleSDK-3.0-dp1/Pebble/common'
+PREFIX = '/usr/local'
+TARGET_PLATFORMS = [u'aplite', u'basalt']
